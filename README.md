eeaf558c1e02b66cd34535d74f85774c9338bcbb
JJ Shep <thefile.market@gmail.com>
2023-03-02 13:23:07 -0500

    Add dotfiles and install script

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # dotfiles-hypr
-Dark ocean theme for Hyprland
+Dotfiles for a dark ocean theme for Hyprland. Currently only supported on Arch Linux.
+
+![Screenshot](oceanrice.png)
+
+Contains install script for Arch Linux.
+
+Dependencies (installed by script):
+	
+	[AUR] swaylock (Lock screen client)
+	
+	[AUR] hyprland (Tiling window manager)
+
+	[AUR] waybar-hyprland (Bar for Hyprland)
+
+	[AUR] rofi-lbonn-wayland (Rofi fork for wayland)
+
+	[AUR] fastfetch (Fetch tool)
+
+	sddm (Display manager)
+
+	alacritty (Terminal emulator)
+
+	wezterm (Terminal emulator)
+
+	firefox (Browser)
+
+	vim (Text editor)
+
+	ttf-iosevka-nerd (Terminal font)
+
+	swaybg (Wallpaper client)
+
+INSTRUCTIONS:
+
+To install for Arch Linux, run the installation script (`./install-arch.sh`). For other distributions, install required packages and manually move things to their respective directories. Be advised that things may not work properly on distributions other than Arch.
+
+
+CREDITS:
+
+https://github.com/linuxmobile/hyprland-dots (Wezterm configuration)
---
+++ install-arch.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# Check root
+if [ "$EUID" == 0 ]
+  then echo "This script must be run as a standard user, otherwise configuration files will be installed in /root."
+  exit
+fi
+
+# Install dotfiles
+mkdir ~/.config 2>/dev/null
+cp -r .config/* ~/.config/
+cp -r Pictures ~/
+
+install_aur () {
+	if sudo pacman -Q | grep -q $1 ; then
+		echo "$1 already installed, skipping."
+	else
+		mkdir -p ~/misc/clone
+		cd ~/misc/clone
+		git clone https://aur.archlinux.org/$1.git
+		cd $1
+		makepkg -si
+		cd ~/misc
+		rm clone -rf
+	fi
+}
+
+# Install dependencies
+sudo pacman -S alacritty wezterm firefox vim ttf-iosevka-nerd swaybg base-devel sddm --needed
+
+# Install AUR packages
+while true; do
+read -p "Are you using Nvidia? (y/n) " yn
+case $yn in
+	y ) install_aur hyprland-nvidia;
+		break;;
+	n ) install_aur hyprland;
+		break;;
+	* ) echo "Invalid response.";;
+esac
+done
+
+install_aur swaylock
+install_aur waybar-hyprland
+install_aur rofi-lbonn-wayland
+install_aur fastfetch
+
+# Finish
+echo "Installation complete."
